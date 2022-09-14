27b305cc69b259f5319b0e7af60940f03b4c285a
Fernando Medina Corey <fmcorey@gmail.com>
2022-09-14 15:14:46 -0700

    Hotfix, fix notifications endpoint

--- frontend/src/store/modules/notifications.js
+++ frontend/src/store/modules/notifications.js
@@ -10,10 +10,7 @@ export default {
   actions: {
     async update (context, { accountId, deviceId }) {
       const providerId = context.rootState.providerId
-      // End of next like should be updated to this at the end insread of deviceId:
-      //  &accountId=${accountId}`
-      // Needs to be done after the notifications service is migrated over though
-      const url = `${notificationsUrl}notifications?providerId=${providerId}&deviceId=${accountId}`
+      const url = `${notificationsUrl}notifications?providerId=${providerId}&accountId=${accountId}`
       const result = await axios.get(url)
       const notifications = result.data.items
       for (let i = 0; i < notifications.length; i++) {
